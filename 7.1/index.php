e3c0ef34b1e0dd8f7abe33c9bc04358210452822
BastiaanvanDelft <9019711@student.zadkine.nl>
2024-09-13 14:36:34 +0200

    yes

---
+++ 7.1/index.php
@@ -0,0 +1,8 @@
+<?php
+?>
+
+<form method="POST" action="process.php">
+    Geboortedatum: <input type="date" name="date"><br>
+    Telefoonnummer: <input type="tel" name="tel"><br>
+    <input type="submit" value="send">
+</form>
---
+++ 7.1/process.php
@@ -0,0 +1,6 @@
+<?php
+// verwerk.php
+$date = $_POST['date'];
+$tel = $_POST['tel'];
+echo "Geboortedatum: " . $date . "<br>";
+echo "Telefoonnummer: " . $tel;
